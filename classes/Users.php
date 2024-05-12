996adcf4f57a7ba087ab193ada192ef22e2686b7
ibrahim <ibrahimbircan_12@hotmail.com>
2024-05-12 13:58:19 +0300

    user fetch service created

--- classes/Users.php
+++ classes/Users.php
@@ -1,35 +1,39 @@
 <?php
 
-class Users{
+class Users
+{
 
     public $name;
     public $email;
     public $password;
     public $created_at;
-    
+
     private $conn;
     private $users_table;
 
-    public function __construct($db){
+    public function __construct($db)
+    {
         $this->conn = $db;
         $this->users_table = "kullanicilar";
     }
 
-    public function create_user(){
-        
+    public function create_user()
+    {
+
         $user_query = "INSERT INTO " . $this->users_table . " SET name = ? , email = ? , password = ? , created_at = now()";
 
         $user_obj = $this->conn->prepare($user_query);
 
         $user_obj->bind_param("sss", $this->name, $this->email, $this->password);
 
-        if($user_obj->execute()){
+        if ($user_obj->execute()) {
             return true;
         }
         return false;
     }
 
-    public function check_email(){
+    public function check_email()
+    {
 
         $email_query = "SELECT * FROM " . $this->users_table . " WHERE email = ?";
 
@@ -37,7 +41,7 @@ class Users{
 
         $user_obj->bind_param("s", $this->email);
 
-        if($user_obj->execute()){
+        if ($user_obj->execute()) {
             $data = $user_obj->get_result();
             return $data->fetch_assoc();
         }
@@ -45,24 +49,52 @@ class Users{
         return array();
     }
 
-    public function check_login(){
+    public function check_login()
+    {
 
-        $email_query = "SELECT * from ".$this->users_table." WHERE email = ?";
-    
+        $email_query = "SELECT * from " . $this->users_table . " WHERE email = ?";
+
         $user_obj = $this->conn->prepare($email_query);
-    
+
         $user_obj->bind_param("s", $this->email);
-    
-        if($user_obj->execute()){
-    
-           $data = $user_obj->get_result();
-    
-           return $data->fetch_assoc();
+
+        if ($user_obj->execute()) {
+
+            $data = $user_obj->get_result();
+
+            return $data->fetch_assoc();
         }
-    
+
         return array();
-      }
+    }
 
+    public function verify_token($token, $secret_key)
+    {
+        $token = substr($token, 7); // Remove bearer
+        try {
+            $payload = Token::Verify($token, $secret_key);
+            return $payload;
+        } catch (Exception $e) {
+            return null; // Return null if token is not valid or an error occurs
+        }
+    }
+
+    public function get_user_by_email()
+    {
+
+        $email_query = "SELECT * from " . $this->users_table . " WHERE email = ?";
+
+        $user_obj = $this->conn->prepare($email_query);
+
+        $user_obj->bind_param("s", $this->email);
+
+        if ($user_obj->execute()) {
+
+            $data = $user_obj->get_result();
+
+            return $data->fetch_assoc();
+        }
+
+        return array();
+    }
 }
-
-?>
---
+++ v1/get-user.php
@@ -0,0 +1,75 @@
+<?php
+
+ini_set('display_errors', 1);
+
+//include headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+
+//include files
+include_once("../config/database.php");
+include_once("../classes/Users.php");
+include_once("../jwt/token.php");
+
+//objects
+$db = new Database();
+$connection = $db->connect();
+$user_obj = new Users($connection);
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    if (!empty($data->email)) {
+
+        $user_obj->email = $data->email;
+
+        $user_data = $user_obj->get_user_by_email();
+
+        if (!empty($user_data)) {
+
+            // Token'ı al
+            $headers = apache_request_headers();
+            $token = $headers['Authorization'];
+            // Secret key
+            $secret_key = "qwe1234";
+            // Token'ı doğrula
+            $payload = $user_obj->verify_token($token, "q");
+
+            // Eğer token geçerli değilse veya bir hata varsa
+            if (!$payload) {
+                http_response_code(404);
+                echo json_encode(array(
+                    "status" => 0,
+                    "message" => "Invalid token"
+                ));
+                exit(); // İşlemi sonlandır
+            }
+            $user_arr_data = array(
+                "id" => $user_data['id'],
+                "name" => $user_data['name'],
+                "email" => $user_data['email']
+            );
+
+            http_response_code(200);
+            echo json_encode(array(
+                "status" => 1,
+                "message" => "Get User successfully",
+                "user" => $user_arr_data
+            ));
+        } else {
+            http_response_code(404);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "Invalid user"
+            ));
+        }
+    } else {
+        http_response_code(404);
+        echo json_encode(array(
+            "status" => 0,
+            "message" => "All data needed"
+        ));
+    }
+}
