91ce207bb17db6341803f51312e42b1e4298b34f
ibrahim <ibrahimbircan_12@hotmail.com>
2024-05-24 13:55:14 +0300

    get-user API service has changed.

--- v1/get-user.php
+++ v1/get-user.php
@@ -17,64 +17,38 @@ $db = new Database();
 $connection = $db->connect();
 $user_obj = new Users($connection);
 
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
+if ($_SERVER['REQUEST_METHOD'] === "GET") {
+    //get token
+    $headers = apache_request_headers();
+    $token = $headers['Authorization'];
+    // Secret key
+    $secret_key = "qwe1234";
+    // Verify token
+    $payload = $user_obj->verify_token($token, $secret_key);
 
-    $data = json_decode(file_get_contents("php://input"));
-
-    // Check if the data is not empty
-    if (!empty($data->email)) {
-
-        $user_obj->email = $data->email;
-
-        $user_data = $user_obj->get_user_by_email();
-
-        // If the user is not empty
-        if (!empty($user_data)) {
-
-            //get token
-            $headers = apache_request_headers();
-            $token = $headers['Authorization'];
-            // Secret key
-            $secret_key = "qwe1234";
-            // Verify token
-            $payload = $user_obj->verify_token($token, $secret_key);
-
-            // If the token is not valid or there is an error
-            if (!$payload) {
-                http_response_code(404);
-                echo json_encode(array(
-                    "status" => 0,
-                    "message" => "Invalid token"
-                ));
-                exit(); // End process
-            }
-
-            $user_arr_data = array(
-                "id" => $user_data['id'],
-                "name" => $user_data['name'],
-                "email" => $user_data['email']
-            );
-
-            http_response_code(200);
-            echo json_encode(array(
-                "status" => 1,
-                "message" => "Get User successfully",
-                "user" => $user_arr_data
-            ));
-        } else { // If the user is empty
-            http_response_code(404);
-            echo json_encode(array(
-                "status" => 0,
-                "message" => "Invalid user"
-            ));
-        }
-    } else { // If the data is empty
+    // If the token is not valid or there is an error
+    if (!$payload) {
         http_response_code(404);
         echo json_encode(array(
             "status" => 0,
-            "message" => "All data needed"
+            "message" => "Invalid token"
         ));
+        exit(); // End process
     }
+
+    $user_arr_data = array(
+        "id" => $payload['id'],
+        "name" => $payload['name'],
+        "email" => $payload['email']
+    );
+
+    http_response_code(200);
+    echo json_encode(array(
+        "status" => 1,
+        "message" => "Get User successfully",
+        "data" => $user_arr_data
+    ));
+
 } else { // If the request method is not POST
     http_response_code(503);
     echo json_encode(array(
